d7bf6e488ab80fe1bd3d855cc175caa5021bc93f
gitseoyeon <tjdus553926@gmail.com>
2025-11-05 02:24:59 +0900

    feat: s3 연결 설정

--- src/main/java/org/example/stamppaw_backend/common/exception/ErrorCode.java
+++ src/main/java/org/example/stamppaw_backend/common/exception/ErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    COMPANION_NOT_FOUND(HttpStatus.NOT_FOUND, "동행글을 찾을 수 없습니다.")
+    COMPANION_NOT_FOUND(HttpStatus.NOT_FOUND, "동행글을 찾을 수 없습니다."),
 
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 파일 업로드에 실패했습니다.")
     ;
 
     private final HttpStatus httpStatus;
