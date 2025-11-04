fb325b0a1bf696e8c6583b654ae27f2d2f341c3a
gitseoyeon <tjdus553926@gmail.com>
2025-11-05 00:26:11 +0900

    add: 동행자 관련 엔티티 작성 및 추가

--- src/main/java/org/example/stamppaw_backend/companion/entity/Companion.java
+++ src/main/java/org/example/stamppaw_backend/companion/entity/Companion.java
@@ -1,4 +1,48 @@
 package org.example.stamppaw_backend.companion.entity;
 
-public class Companion {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.stamppaw_backend.common.BasicTimeEntity;
+import org.example.stamppaw_backend.user.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "companions")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Companion extends BasicTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private RecruitmentStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "companion", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CompanionApply> applies = new ArrayList<>();
+
+    @Override
+    public void setPrePersist() {
+        super.setPrePersist();
+        this.status = RecruitmentStatus.ONGOING;
+    }
+
 }
