207bd3ccde26e0802637d9eef6be378eab8f04c6
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2025-11-05 17:28:26 +0900

    Feat: 동행글 작성 및 조회 (#10)
    * add: 동행자 관련 엔티티 작성 및 추가
    
    * feat: 동행 모집 글 생성
    
    * feat: 동행글 목록, 상세 조회
    
    * remove: 불필요한 클래스 제거
    
    * feat: s3 연결 설정
    
    * feat: 동행글 생성시 이미지 업로드 추가
    
    * feat: 유저 정보 반환하는 메소드 추가
    
    * feat: 글 생성 및 조회시 유저 정보 추가
    
    * remove: 중복된 코드 제거

--- src/main/java/org/example/stamppaw_backend/companion/dto/response/CompanionResponse.java
+++ src/main/java/org/example/stamppaw_backend/companion/dto/response/CompanionResponse.java
@@ -1,4 +1,41 @@
 package org.example.stamppaw_backend.companion.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.stamppaw_backend.companion.entity.Companion;
+import org.example.stamppaw_backend.companion.entity.RecruitmentStatus;
+import org.example.stamppaw_backend.user.dto.UserDto;
+import org.example.stamppaw_backend.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanionResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String image;
+    private LocalDateTime registeredAt;
+    private LocalDateTime modifiedAt;
+    private RecruitmentStatus status;
+    private UserDto user;
+
+
+    public static CompanionResponse fromEntity(Companion companion) {
+        return CompanionResponse.builder()
+                .id(companion.getId())
+                .title(companion.getTitle())
+                .content(companion.getContent())
+                .image(companion.getImageUrl())
+                .registeredAt(companion.getRegisteredAt())
+                .modifiedAt(companion.getModifiedAt())
+                .status(companion.getStatus())
+                .user(UserDto.fromEntity(companion.getUser()))
+                .build();
+    }
 }
