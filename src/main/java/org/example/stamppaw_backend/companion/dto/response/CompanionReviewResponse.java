4274b50250025ce3f1cd07ef96e9f77f492da22a
gitseoyeon <tjdus553926@gmail.com>
2025-11-13 17:23:19 +0900

    feat: 받은 리뷰 목록 조회

--- src/main/java/org/example/stamppaw_backend/companion/dto/response/CompanionReviewResponse.java
+++ src/main/java/org/example/stamppaw_backend/companion/dto/response/CompanionReviewResponse.java
@@ -1,4 +1,30 @@
 package org.example.stamppaw_backend.companion.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.stamppaw_backend.companion.entity.CompanionReview;
+import org.example.stamppaw_backend.user.dto.response.UserDto;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanionReviewResponse {
+    private String title;
+    private UserDto userDto;
+    private List<String> tags;
+
+    public static CompanionReviewResponse from(CompanionReview companionReview) {
+        return CompanionReviewResponse.builder()
+                .title(companionReview.getApply().getCompanion().getTitle())
+                .userDto(UserDto.fromEntity(companionReview.getApply().getCompanion().getUser()))
+                .tags(companionReview.getTags().stream()
+                        .map(m -> m.getTag().getTag())
+                        .toList())
+                .build();
+    }
 }
