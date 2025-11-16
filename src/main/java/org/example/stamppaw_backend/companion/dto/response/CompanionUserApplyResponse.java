28c94f08f143491f7062d3fc015f8a4f7457f5c1
gitseoyeon <tjdus553926@gmail.com>
2025-11-17 01:55:28 +0900

    add: 신청한 동행글 id 값 추가

--- src/main/java/org/example/stamppaw_backend/companion/dto/response/CompanionUserApplyResponse.java
+++ src/main/java/org/example/stamppaw_backend/companion/dto/response/CompanionUserApplyResponse.java
@@ -13,6 +13,7 @@ import org.example.stamppaw_backend.companion.entity.CompanionApply;
 @AllArgsConstructor
 public class CompanionUserApplyResponse {
     private Long id;
+    private Long companionId;
     private CompanionDto companionDto;
     private String status;
     private boolean isReviewWritten;
@@ -20,6 +21,7 @@ public class CompanionUserApplyResponse {
     public static CompanionUserApplyResponse from(CompanionApply apply, boolean isReviewWritten) {
         return CompanionUserApplyResponse.builder()
                 .id(apply.getId())
+                .companionId(apply.getCompanion().getId())
                 .companionDto(CompanionDto.from(apply.getCompanion()))
                 .status(apply.getStatus().toString())
                 .isReviewWritten(isReviewWritten)
