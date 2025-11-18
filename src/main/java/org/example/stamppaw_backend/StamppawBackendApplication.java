f70695196131034134cbf0e46bfbd0b29e9e968e
gitseoyeon <tjdus553926@gmail.com>
2025-11-18 15:24:20 +0900

    fix: scanBasePackages 설정 추가

--- src/main/java/org/example/stamppaw_backend/StamppawBackendApplication.java
+++ src/main/java/org/example/stamppaw_backend/StamppawBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example.stamppaw_backend")
 public class StamppawBackendApplication {
 
     public static void main(String[] args) {
