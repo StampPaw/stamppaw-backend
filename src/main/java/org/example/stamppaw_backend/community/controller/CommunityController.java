993066d5e843e84faa76a30c7adcc028e096ec36
gitseoyeon <tjdus553926@gmail.com>
2025-11-21 13:31:29 +0900

    add: 필요한 클래스 추가

---
+++ src/main/java/org/example/stamppaw_backend/community/controller/CommunityController.java
@@ -0,0 +1,4 @@
+package org.example.stamppaw_backend.community.controller;
+
+public class CommunityController {
+}
---
+++ src/main/java/org/example/stamppaw_backend/community/service/CommunityService.java
@@ -0,0 +1,4 @@
+package org.example.stamppaw_backend.community.service;
+
+public class CommunityService {
+}
